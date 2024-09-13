bc4ef76e21a56fe242f94e0a53946958727002a1
DariaReyderman <dariaivanova0033@gmail.com>
2024-09-13 13:24:58 +0300

    hw5_challenge.py

--- hw5_challenge.py
+++ hw5_challenge.py
@@ -3,7 +3,7 @@
 counter: int = 1
 age: float = float(input("How old are you? "))
 
-while age >= 18 and counter < 10:
+while age >= 18 and counter <= 10:
     counter += 1
     print("Enjoy your beer! ")
     beers_left: int = int(input("Beers left: "))
